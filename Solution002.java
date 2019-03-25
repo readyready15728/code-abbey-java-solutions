8664827f59f0dc613b975304d0871eb19794cfaf
Lynn Bradshaw <readyready15728@gmail.com>
2019-03-25 13:10:30 -0400

    Adding solution #2

---
+++ Solution002.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Solution002 {
+  public static void main(String[] args) {
+    List<String> input = Common.getFileAsLines("test-data002.txt");
+    int sum = 0;
+
+    for (String str : input.get(1).split(" ")) {
+      sum += Integer.parseInt(str.strip());
+    }
+
+    System.out.println(sum);
+  }
+}

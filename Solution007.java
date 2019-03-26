67e0ff68e4d396de1ae0068c22a65bd5eb49ca31
Lynn Bradshaw <readyready15728@gmail.com>
2019-03-26 18:47:45 -0400

    Adding solution #7

---
+++ Solution007.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Solution007 {
+  public static void main(String[] args) throws IOException {
+    String input = Common.getFileAsString("test-data007.txt");
+    String[] textNumerals = input.split(" ");
+
+    for (int i = 1; i < textNumerals.length; i++) {
+      System.out.print((int) Math.round((((float) Integer.parseInt(textNumerals[i].strip())) - 32) * 5.0 / 9.0));
+
+      if (i != textNumerals.length - 1) {
+        System.out.print(' ');
+      }
+    }
+
+    System.out.println();
+  }
+}

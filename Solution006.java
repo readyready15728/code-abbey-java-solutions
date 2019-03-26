18f6c0c42bb0081c37e1292a1a716ca0f46db09d
Lynn Bradshaw <readyready15728@gmail.com>
2019-03-26 18:47:19 -0400

    Adding solution #6

---
+++ Solution006.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution006 {
+  public static void main(String[] args) throws IOException {
+    List<String> input = Common.getFileAsLines("test-data006.txt");
+    List<Integer> output = new ArrayList<Integer>();
+
+    input.remove(0);
+
+    for (String row : input) {
+      String[] textIntegers = row.split(" ");
+      int a = Integer.parseInt(textIntegers[0]);
+      int b = Integer.parseInt(textIntegers[1]);
+
+      float quotient = (float) a / (float) b;
+      output.add((int) Math.round(quotient));
+    }
+
+    for (int i = 0; i < output.size(); i++) {
+      System.out.print(output.get(i));
+
+      if (i != output.size() - 1) {
+        System.out.print(' ');
+      }
+    }
+
+    System.out.println();
+  }
+}

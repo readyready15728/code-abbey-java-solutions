e8f849bf741807edd14fcfdb1fd59021e803da0c
Lynn Bradshaw <readyready15728@gmail.com>
2019-03-29 17:57:08 -0400

    Changing variable name in solution #7

--- Solution007.java
+++ Solution007.java
@@ -3,12 +3,12 @@ import java.io.*;
 public class Solution007 {
   public static void main(String[] args) throws IOException {
     String input = Common.getFileAsString("test-data007.txt");
-    String[] textNumerals = input.split(" ");
+    String[] textIntegers = input.split(" ");
 
-    for (int i = 1; i < textNumerals.length; i++) {
-      System.out.print((int) Math.round((((float) Integer.parseInt(textNumerals[i])) - 32) * 5.0 / 9.0));
+    for (int i = 1; i < textIntegers.length; i++) {
+      System.out.print((int) Math.round((((float) Integer.parseInt(textIntegers[i])) - 32) * 5.0 / 9.0));
 
-      if (i != textNumerals.length - 1) {
+      if (i != textIntegers.length - 1) {
         System.out.print(' ');
       }
     }

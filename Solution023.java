e6a5d90c666fd345bcea7d925eaf0c7bf75faf47
Lynn Bradshaw <readyready15728@gmail.com>
2019-04-04 16:24:12 -0400

    Adding solution #23

---
+++ Solution023.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Solution023 {
+  public static void main(String[] args) throws IOException {
+    List<String> input = Common.getFileAsLines("test-data023.txt");
+    List<Integer> integers = new ArrayList<Integer>();
+    List<Integer> output = new ArrayList<Integer>();
+    int swaps = 0;
+
+    for (String textInteger : input.get(0).split(" ")) {
+      integers.add(Integer.parseInt(textInteger));
+    }
+
+    for (int i = 0; i < integers.size() - 2; i++) {
+      if (integers.get(i + 1) < integers.get(i)) {
+        int temp = integers.get(i + 1);
+        
+        integers.set(i + 1, integers.get(i));
+        integers.set(i, temp);
+
+        swaps++;
+      }
+    }
+
+    output.add(swaps);
+    
+    BigInteger checksum = new BigInteger(Integer.toString(0));
+    BigInteger multiplicand = new BigInteger("113");
+    BigInteger modulus = new BigInteger("10000007");
+
+    for (int i = 0; i < integers.size() - 1; i++) {
+      BigInteger converted = new BigInteger(String.valueOf(integers.get(i)));
+      
+      checksum = checksum.add(converted);
+      checksum = checksum.multiply(multiplicand);
+      checksum = checksum.mod(modulus);
+    }
+
+    output.add(checksum.intValue());
+
+    for (int i = 0; i < output.size(); i++) {
+      System.out.print(output.get(i));
+
+      if (i != output.size() - 1) {
+        System.out.print(' ');
+      }
+    }
+
+    System.out.println();
+  }
+}
